24177f8c57016daaa4b1b9a66d2accc9c15570fd
arkB <araki.knsk@gmail.com>
2023-02-23 16:42:27 +0900

    Add install.sh

--- reply.sh
+++ reply.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ps aux | grep reply.py | grep -v grep | awk '{ print "kill -9", $2 }' | sh
-cd /home/pi/work/morazubot/v4
+cd /home/pi/work/morazubot_v4
 source v4env/bin/activate
 sleep 1
 python reply.py &
